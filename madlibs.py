4af406a13717d83dd3c2623e91b66158cf6da1a7
SAMI <sami@Apples-MacBook-Pro.local>
2017-12-03 00:50:57 +0530

    Day 2-Add mad libs gamee

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+Noun = input("Enter noun")
+Adverb = input(" Enter an adverb")
+Adjective = input("Enter an adjective")
+preposition = input("Enter preposition")
+story_template = 'Hello '+Noun+' Once upon a time we  '+Adverb+ ' and there was a '+ Adjective+' boy who sat '+preposition+' tree '
+
+print(story_template)
+
+
+
